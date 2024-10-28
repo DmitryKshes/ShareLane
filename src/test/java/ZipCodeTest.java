099d69b319a4974ef27c9738705ef1b2fd2f9161
DmitryKshes <dmitriykshes@gmail.com>
2024-10-28 12:40:27 +0300

    HW2 add zip code tests

---
+++ src/test/java/ZipCodeTest.java
@@ -0,0 +1,82 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ZipCodeTest {
+
+    @Test
+    public void test1() {
+        /*
+       1. Открыть браузер.
+       2. Открыть страницу https://www.sharelane.com/cgi-bin/register.py .
+       3. В поле Zip code ввести 1111.
+       4. Нажать кнопку continue.
+       5. Проверить отображение текста ошибки.
+       6. Закрыть браузер.
+        */
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("1111");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class = error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+        browser.quit();
+    }
+
+    @Test
+    public void test2() {
+        /*
+       1. Открыть браузер.
+       2. Открыть страницу https://www.sharelane.com/cgi-bin/register.py .
+       3. В поле Zip code ввести test.
+       4. Нажать кнопку continue.
+       5. Проверить отображение текста ошибки.
+       6. Закрыть браузер.
+        */
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("test");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class = error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits", "incorrect text");
+        browser.quit();
+    }
+
+    @Test
+    public void test3() {
+        /*
+       1. Открыть браузер.
+       2. Открыть страницу https://www.sharelane.com/cgi-bin/register.py .
+       3. Нажать кнопку continue.
+       4. Проверить отоюражение текста ошибки.
+       5. Закрыть браузер.
+        */
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class = error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits", "incorrect text");
+        browser.quit();
+    }
+
+    @Test
+    public void test4() {
+        /*
+       1. Открыть браузер.
+       2. Открыть страницу https://www.sharelane.com/cgi-bin/register.py .
+       3. В поле Zip code ввести 11111.
+       4. Нажать кнопку continue.
+       5. Проверить переход на страницу регистрации.
+       6. Закрыть браузер.
+        */
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.sharelane.com/cgi-bin/register.py");
+        browser.findElement(By.name("zip_code")).sendKeys("11111");
+        browser.findElement(By.cssSelector("[value = Continue]")).click();
+        String getUrl = browser.getCurrentUrl();
+        Assert.assertEquals(getUrl, "https://www.sharelane.com/cgi-bin/register.py?page=1&zip_code=11111", "Not registration page");
+        browser.quit();
+    }
+}
